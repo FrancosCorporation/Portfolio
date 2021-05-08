f455cf4d08101d8ed7ac627db7f6c81fc339e340
FrancoCorporation <rodolfofrancoxico@gmail.com>
2021-05-08 00:53:40 -0300

    Projetos antigos sendo enviados !

---
+++ Projetos-Pessoais_Comeco_da_Carreir/C++/Novo Projeto/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 21 de abr de 2020
+ *      Author: Rodolfo
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout <<"Rodolfinho lindu";
+
+	return 0;
+}
+
+
+
